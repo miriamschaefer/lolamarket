9217dc1db9627bad812f0e559932131b49e181a1
miriamschaefer <msuarezmelian@gmail.com>
2020-11-01 19:57:10 +0100

    get all the data from api, FINALLY

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,38 @@
 import React, {useState, useEffect} from 'react';
 import '../stylesheets/App.scss';
-import getData from '../services/Api';
+import  { getToken, getShops, getCategories } from '../services/Api';
 
 
 
 const App = (props) => {
 
-const initialState = {
-  
-}
+const store_id = 1;
+let postalCode = 28010;
 
+const [token, setToken] = useState('');
+const [shops, setShops] = useState([]);
+const [categories, setCategories] = useState([]);
 
-const [token, setToken] = useState('')
 
 useEffect(() => {
-  getData().then((token) => {
-   setToken({ token })
- });
-}, [])
+  if (token) {
+    getShops(token, postalCode).then((shops) => setShops(shops))
+    getCategories(token, postalCode, store_id).then((categories) => setCategories(categories.categories))
+  } else {
+    getToken().then((data) => {
+      setToken(data.token)
+    })
+  }
+}, [token, postalCode]);
 
-
-if (token) {
-  console.log(token)
-}
+console.log(token);
+console.log(shops);
+console.log(categories);
 
 
 return (
   <div className="App">
-    Holi 
+Holi
   </div>
 )
 
--- src/services/Api.js
+++ src/services/Api.js
@@ -8,9 +8,6 @@ const request = new Request('https://api.comprea.com/v7/user/session', {
     headers,
 });
 
-const postalCode = 28010;
-const store_id = 1;
-// const category_id = 93;
 
 const postalRequest = 'https://api.comprea.com/v7/user/postalcode';
 
@@ -19,34 +16,48 @@ const shopRequest = 'https://api.comprea.com/v7/company/categories';
 // const productRequest = 'https://api.comprea.com/v7/company/items';
 
 
-const getData = async () => {
+const getToken = async () => {
 
   const getToken = await fetch(request);
   const token = await getToken.json();
-  console.log(token);
 
-  const getShops = await fetch(`${postalRequest}?token=${token.token}&postalcode=${postalCode}`);
+  return token;
+
+}
+
+
+const getShops = async (token, postalCode) => {
+
+ const getShops = await fetch(`${postalRequest}?token=${token}&postalcode=${postalCode}`);
   const shops = await getShops.json();
-  console.log(shops);
+  
+    return shops.services[1].markets.map((market) => {
+      return {
+        id: market.id,
+        name: market.name,
+        shortcut: market.shortcut,
+        icon: market.icon,
+        color: market.color,
+      }
+    })
 
-  const getCategories = await fetch(`${shopRequest}?token=${token.token}&postalcode=${postalCode}&company_id=${store_id}`);
+}
+
+
+const getCategories = async (token, postalCode, store_id) => {
+
+  const getCategories = await fetch(`${shopRequest}?token=${token}&postalcode=${postalCode}&company_id=${store_id}`);
   const categories = await getCategories.json();
-  console.log(categories);
-
-  // const getSubcategories = await fetch(`https://api.comprea.com/v7/company/items?token=${token.token}&postalcode=${postalCode}&company_id=${store_id}&category_id=${category_id}`);
-  // const subcategories = await getSubcategories.json();
-  // console.log(subcategories);
-
-  }
+  return categories;
+ }
 
 
-// const getToken = async () => {
-//       const response = await fetch(request);
-//       const token = await response.json();
-//       return token.token;
-
-//     };
+  // // const getSubcategories = await fetch(`https://api.comprea.com/v7/company/items?token=${token.token}&postalcode=${postalCode}&company_id=${store_id}&category_id=${category_id}`);
+  // // const subcategories = await getSubcategories.json();
+  // // console.log(subcategories);
 
 
 
-export default getData;
+
+
+export {  getToken, getShops, getCategories };
